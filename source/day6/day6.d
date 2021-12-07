5e56f26fed6fddaa15a76780387823b980f03903
Ben Konz <benk@spotify.com>
2021-12-06 21:50:24 -0500

    refactoring

--- source/day6/day6.d
+++ source/day6/day6.d
@@ -17,39 +17,41 @@ ulong[] parse_input(string[] lines)
 ulong simulate(ulong days, ulong[] ages)
 {
     ulong[ulong] map;
-    foreach (age; ages) {
-        if (age in map) {
+    foreach (age; ages)
+    {
+        if (age in map)
+        {
             map[age]++;
-        } else {
+        }
+        else
+        {
             map[age] = 1;
         }
     }
 
-    for (int i = 0; i < 9; i++) {
-        if (!(i in map)) {
+    for (int i = 0; i < 9; i++)
+    {
+        if (!(i in map))
+        {
             map[i] = 0;
         }
     }
 
     for (ulong i = 0; i < days; i++)
     {
-       auto babies = map[0];
-       map[0] = map[1];
-       map[1] = map[2];
-       map[2] = map[3];
-       map[3] = map[4];
-       map[4] = map[5];
-       map[5] = map[6];
-       map[6] = map[7] + babies;
-       map[7] = map[8];
-       map[8] = babies;
+        auto babies = map[0];
+        map[0] = map[1];
+        map[1] = map[2];
+        map[2] = map[3];
+        map[3] = map[4];
+        map[4] = map[5];
+        map[5] = map[6];
+        map[6] = map[7] + babies;
+        map[7] = map[8];
+        map[8] = babies;
     }
 
-    ulong sum = 0;
-    foreach (key, value; map) {
-        sum += value;
-    }
-    return sum;
+    return map.values.sum;
 }
 
 ulong a(ulong[] ages)
