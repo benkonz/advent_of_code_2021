02510564ad4665c32d89e204914ee39918c642fc
Ben Konz <benk@spotify.com>
2021-12-08 10:57:46 -0500

    cleanup

--- source/day6/day6.d
+++ source/day6/day6.d
@@ -16,42 +16,32 @@ ulong[] parse_input(string[] lines)
 
 ulong simulate(ulong days, ulong[] ages)
 {
-    ulong[ulong] map;
+    ulong[9] ageToChildren;
     foreach (age; ages)
     {
-        if (age in map)
-        {
-            map[age]++;
-        }
-        else
-        {
-            map[age] = 1;
-        }
+        ageToChildren[age]++;
     }
 
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < days; i++)
     {
-        if (!(i in map))
-        {
-            map[i] = 0;
-        }
+        auto babies = ageToChildren[0];
+        ageToChildren[0] = ageToChildren[1];
+        ageToChildren[1] = ageToChildren[2];
+        ageToChildren[2] = ageToChildren[3];
+        ageToChildren[3] = ageToChildren[4];
+        ageToChildren[4] = ageToChildren[5];
+        ageToChildren[5] = ageToChildren[6];
+        ageToChildren[6] = ageToChildren[7] + babies;
+        ageToChildren[7] = ageToChildren[8];
+        ageToChildren[8] = babies;
     }
 
-    for (ulong i = 0; i < days; i++)
-    {
-        auto babies = map[0];
-        map[0] = map[1];
-        map[1] = map[2];
-        map[2] = map[3];
-        map[3] = map[4];
-        map[4] = map[5];
-        map[5] = map[6];
-        map[6] = map[7] + babies;
-        map[7] = map[8];
-        map[8] = babies;
+    ulong sum = 0;
+    foreach (children; ageToChildren) {
+        sum += children;
     }
 
-    return map.values.sum;
+    return sum;
 }
 
 ulong a(ulong[] ages)
